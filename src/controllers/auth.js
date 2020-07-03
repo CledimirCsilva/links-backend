14eb9db0410458876d8e4e79c8c6328db151c98d
Cledimir C Silva <csilva@ccsweb.com.br>
2020-07-03 17:08:54 -0300

    Storing Accounts into database

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const { Account } = require('../models')
 
 const router = express.Router();
+
+const saltRounds = 10;
 
 router.get('/sign-in', (req, res) => {
     return res.json('Sign In');
 });
 
-router.get('/sign-up', (req, res) => {
-    return res.json('Sign Up');
+router.get('/sign-up', async (req, res) => {
+
+    const email = 'teste@teste.com';
+    const password = '123456';
+    const hash = bcrypt.hashSync(password, saltRounds)
+    const result = await Account.create({email, password:  hash});
+    res.send(result)
 });
 
 module.exports = router;
